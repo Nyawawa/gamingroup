87f652fa463ab93eeea6eff61ec9d9e73a73885f
eiko <eiko@fsmpi.rwth-aachen.de>
2016-01-03 13:26:01 +0000

    change 404

--- 404.php
+++ 404.php
@@ -15,11 +15,13 @@ get_header(); ?>
 			<div id="post-0" class="post error404 not-found">
 				<h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
 				<div class="entry-content">
+<p><img src="https://gg-ac-eiko.c9users.io/wp-content/uploads/2016/01/giphy.gif"><img src="https://gg-ac-eiko.c9users.io/wp-content/uploads/2016/01/Picachu-crying-pokemon.gif"></p>
 					<p><?php _e( "Du scheiß Vollidiot.", 'twentyten' ); ?></p>
 					<p><?php _e( "Man ey!", 'twentyten' ); ?></p>
 					<p><?php _e( "Du hast schon wieder alles zerstört.", 'twentyten' ); ?></p>
 					<p><?php _e( "Mach doch mal wenigstens eine einzige verdammte Sache richtig.", 
 						'twentyten' ); ?></p>
+					
 				</div><!-- .entry-content -->
 			</div><!-- #post-0 -->
 
